dfa55c952b0ee0af288d7cfb68f719542be02377
sudip paladhi <sudippaladhi@gmail.com>
2023-10-10 23:32:34 +0530

    API data updated

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect } from 'react'
-import { YOUTUBE_VIDEO_API } from '../utils/contants';
+import { YOUTUBE_VIDEOS_API } from '../utils/contants';
 const VideoContainer = () => {
 
     useEffect(() => {
@@ -8,7 +8,7 @@ const VideoContainer = () => {
     }, []);
 
     const getYoutubrData = async () => {
-        const data = await fetch(YOUTUBE_VIDEO_API);
+        const data = await fetch(YOUTUBE_VIDEOS_API);
         const json = await data.json();
         console.log(json);
     }
