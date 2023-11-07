cada69b033dcc1c6e7f06ee3ce937c0db2e1a9ee
sudip paladhi <sudippaladhi@gmail.com>
2023-11-08 00:08:16 +0530

    Live chat updated

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import ChatMessage from './ChatMessage'
+import React, { useEffect } from "react";
+import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { getRandomMessage, getRandomName } from "../utils/helper";
 
 const LiveChat = () => {
+  const dispatch = useDispatch();
+  const chatMessages = useSelector((store) => store.chat.messages);
+  useEffect(() => {
+   const i = setInterval(() => {
+console.log("API CALLING");
+
+dispatch(addMessage({
+      name: getRandomName(),
+      message: getRandomMessage(25) + ".",
+    })
+    );
+    }, 2000);
+    return () => clearInterval(i);
+  }, []);
+
   return (
-    <div className='w-full h-[433px] p-1 rounded border border-black'>
-    <ChatMessage name="Sudip paladhi" message="welcome" />
+    <div className="w-full h-[433px] p-1 rounded border border-black overflow-y-scroll flex flex-col-reverse">
+    <div>
+      {
+        chatMessages.map((c, i) => (
+        <ChatMessage key={i} name={c.name} message={c.message} />
+        ))
+    }
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default LiveChat;
---
+++ src/utils/chatSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Live_Chat_Count } from "./contants";
+
+const chatSlice = createSlice({
+    name: "chat",
+    initialState: {
+        messages:[],
+    },
+
+    reducers: {
+        addMessage : (state, action) => {
+            state.messages.splice(Live_Chat_Count, 1);
+            state.messages.push(action.payload);
+        },
+    },
+});
+
+export const { addMessage } = chatSlice.actions;
+export default chatSlice.reducer;
