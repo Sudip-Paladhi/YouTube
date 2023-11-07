5a48dc9a588225c8d9b3ce6f9ecf3b4b295c0aa7
sudip paladhi <sudippaladhi@gmail.com>
2023-11-08 00:53:57 +0530

    Live chat updated

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -3,8 +3,10 @@ import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { getRandomMessage, getRandomName } from "../utils/helper";
+import { useState } from "react";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
   const chatMessages = useSelector((store) => store.chat.messages);
   useEffect(() => {
@@ -21,6 +23,7 @@ dispatch(addMessage({
   }, []);
 
   return (
+    <>
     <div className="w-full h-[433px] p-1 rounded border border-black overflow-y-scroll flex flex-col-reverse">
     <div>
       {
@@ -30,6 +33,32 @@ dispatch(addMessage({
     }
       </div>
     </div>
+
+    <form
+        className="w-full p-2 flex border border-black"
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          dispatch(
+            addMessage({
+              name: "Sudip Paladhi",
+              message: liveMessage,
+            })
+          );
+          setLiveMessage("");
+        }}
+      >
+        <input
+          className="px-2 w-96"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+        />
+        <button className="px-2 mx-2 bg-green-100">Send</button>
+      </form>
+    </>
   );
 };
 
